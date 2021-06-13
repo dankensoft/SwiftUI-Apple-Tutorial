53536de73b047a2b58b491cfe16dddc68b8e06be
Danniels Castillo <danniels.magana@gmail.com>
2021-06-13 17:41:05 -0600

    Creating and Combining Views

---
+++ LandmarksDKS/LandmarksDKSApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksDKSApp.swift
+//  LandmarksDKS
+//
+//  Created by Danniels Castillo on 13/06/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksDKSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
