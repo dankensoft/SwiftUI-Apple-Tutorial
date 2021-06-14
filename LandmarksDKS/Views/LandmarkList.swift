92614cd26c79809e4e27235fd1131a8bf410af8c
Danniels Castillo <danniels.magana@gmail.com>
2021-06-13 23:04:55 -0600

    Building Lists and Navigation

--- LandmarksDKS/Views/LandmarkList.swift
+++ LandmarksDKS/Views/LandmarkList.swift
@@ -9,12 +9,26 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List(landmarks) {
+                landmark in
+                NavigationLink(
+                    destination: LandmarkDetail(landmark: landmark)){
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
+    /*ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+        LandmarkList()
+            .previewDevice(PreviewDevice(rawValue: deviceName))
+            .previewDisplayName(deviceName)
+    }*/
         LandmarkList()
     }
 }
