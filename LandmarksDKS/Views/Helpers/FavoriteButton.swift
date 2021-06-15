5308350f2f6ab0e81821123d8b849b73be369521
Danniels Castillo <danniels.magana@gmail.com>
2021-06-15 13:10:46 -0600

    Handling User Input

---
+++ LandmarksDKS/Views/Helpers/FavoriteButton.swift
@@ -0,0 +1,26 @@
+//
+//  FavoriteButton.swift
+//  LandmarksDKS
+//
+//  Created by Danniels Castillo on 15/06/21.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    var body: some View {
+        Button(action: {
+            isSet.toggle()
+        }) {
+            Image(systemName: isSet ? "star.fill" : "star")
+                .foregroundColor(isSet ? Color.yellow : Color.gray)
+        }
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+    }
+}
