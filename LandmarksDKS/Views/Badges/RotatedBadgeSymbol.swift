9eaf05e9ab627354f4660e7678433c2656b3b1cb
Danniels Castillo <danniels.magana@gmail.com>
2021-06-21 22:26:28 -0600

    Drawing Paths and Shapes

---
+++ LandmarksDKS/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  LandmarksDKS
+//
+//  Created by Danniels Castillo on 21/06/21.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
